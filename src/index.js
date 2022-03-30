5b2297b1d1c7b1d8d0649e314b6236de022e0712
Akshay-Singh-Rajput <akshaysinghr98@gmail.com>
2022-03-30 18:08:39 +0530

    nothing

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const cors = require('cors');
-const cartController = require("./controllers/cart.controllers")
+const cartController = require("./controllers/cart.controllers");
+const womensdatasController = require('./controllers/women.controller')
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
 app.use("/carts", cartController)
+app.use("/womensdatas", womensdatasController);
+
 
 module.exports  = app;
